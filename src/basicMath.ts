ec29d3269f445e73fe35505967b01e8d127d34a9
Thalles Passos <contato@thalles.me>
2022-03-31 18:23:36 -0300

    Remove comments

--- src/basicMath.ts
+++ src/basicMath.ts
@@ -1,35 +1,20 @@
 export class BasicMath {
-  /**
-   * Sum two numbers and return the result
-   */
   sum(firstNumber: number, secondNumber: number): number {
     return firstNumber + secondNumber;
   }
 
-  /**
-   * Subtract two numbers
-   */
   subtract(firstNumber: number, secondNumber: number): number {
     return firstNumber - secondNumber;
   }
 
-  /**
-   * Takes the base expression taken to a specified exponent
-   */
   power(base: number, exponent: number): number {
     return base ** exponent;
   }
-
-  /**
-   * Divide two numbers
-   */
+  
   divide(x: number, y: number) {
     return x / y;
   }
 
-  /**
-   * Multiply the first number by the second number and return the result
-   */
   multiply(firstNumber: number, secondNumber: number): number {
     return firstNumber * secondNumber;
   }
