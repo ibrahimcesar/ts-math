4f4ccf7553982886d9ce259e1992db2ba829f3c3
Thalles Passos <contato@thalles.me>
2022-03-31 18:23:55 -0300

    Remove more comments

--- src/advancedMath.ts
+++ src/advancedMath.ts
@@ -1,21 +1,12 @@
 export class AdvancedMath {
-  /**
-   * Get the hypotenuse of two numbers
-   */
   hypotenuse(firstNumber: number, secondNumber: number): number {
     return Math.hypot(firstNumber, secondNumber);
   }
 
-  /**
-   * Returns the cosine of a number
-   */
   cosine(radian: number) {
     return Math.cos(radian);
   }
 
-  /**
-   * Returns the square root of x
-   */
   squareRoot(x: number) {
     return Math.sqrt(x);
   }
